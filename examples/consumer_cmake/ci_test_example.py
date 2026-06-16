8b6f9979d6d06f77eb8d1c0e26301d0235bb3d0b
Carlos Zoido <mrgalleta@gmail.com>
2026-06-16 06:34:37 +0200

    wip

--- examples/consumer_cmake/ci_test_example.py
+++ examples/consumer_cmake/ci_test_example.py
@@ -1,13 +1,5 @@
 import platform
 from test.examples_tools import run
 
-_system = platform.system()
-if _system == "Windows":
-    profile = "windows-msvc"
-elif _system == "Darwin":
-    profile = "macos-clang"
-else:
-    profile = "linux-gcc"
-
-run(f"conan install --profile ../../profiles/{profile} --build=missing")
-run(f"conan build --profile ../../profiles/{profile}")
+run("conan install --profile ../../profiles/ros --build=missing")
+run("conan build --profile ../../profiles/ros")
