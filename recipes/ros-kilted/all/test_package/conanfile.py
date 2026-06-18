81ee194706587ee171395be47e51cb86fd291529
Carlos Zoido <mrgalleta@gmail.com>
2026-06-18 18:21:22 +0200

    wip

--- recipes/ros-kilted/all/test_package/conanfile.py
+++ recipes/ros-kilted/all/test_package/conanfile.py
@@ -51,18 +51,14 @@ class TestPackageConan(ConanFile):
             diag_script = os.path.join(self.build_folder, "diag_rclpy.py")
             with open(diag_script, "w") as _f:
                 _f.write(
-                    "import sys, rclpy._rclpy_pybind11 as m\n"
-                    "print('pyd OK')\n"
-                    "print('init doc:', (m.Context.init.__doc__ or '')[:300])\n"
+                    "import rclpy._rclpy_pybind11 as m\n"
+                    "print('module attrs:', [a for a in dir(m) if not a.startswith('_')])\n"
                     "ctx = m.Context()\n"
-                    "print('Context() OK')\n"
-                    "ctx.init([], False, None)\n"
-                    "print('init(logging=False) OK')\n"
-                    "ctx.shutdown()\n"
-                    "ctx2 = m.Context()\n"
-                    "ctx2.init([], True, None)\n"
-                    "print('init(logging=True) OK')\n"
-                    "ctx2.shutdown()\n"
+                    "print('Context attrs:', [a for a in dir(ctx) if not a.startswith('_')])\n"
+                    "for attr in dir(ctx):\n"
+                    "    doc = getattr(type(ctx), attr, None)\n"
+                    "    if doc and callable(doc):\n"
+                    "        print(f'  {attr}: {(getattr(doc, \"__doc__\", \"\") or \"\")[:120]}')\n"
                 )
             prefix = 'set "RMW_IMPLEMENTATION=rmw_cyclonedds_cpp" && '
             self.run(
