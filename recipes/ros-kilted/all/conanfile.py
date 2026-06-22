1bf0eda7374d43a9486d5f54cdb3876316fc6250
Carlos Zoido <mrgalleta@gmail.com>
2026-06-22 08:03:08 +0200

    wip

--- recipes/ros-kilted/all/conanfile.py
+++ recipes/ros-kilted/all/conanfile.py
@@ -158,7 +158,6 @@ class Ros2KiltedConan(ConanFile):
             # so CMakeDeps generates that component target.
             self.options["boost/*"].without_python = False
             self.options["boost/*"].python_version = str(self.options.python_version)
-            self.options["boost/*"].without_numpy = True
 
     def layout(self):
         # Single-tree colcon workspace: src/, build/, install/, log/ under ros2_ws/
