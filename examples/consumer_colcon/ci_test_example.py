9c11414a4867057cc4570455ad9e3bf6ff8b3660
danimtb <danimanzaneque@gmail.com>
2026-06-25 10:23:24 +0200

    fix colcon args

--- examples/consumer_colcon/ci_test_example.py
+++ examples/consumer_colcon/ci_test_example.py
@@ -1,6 +1,5 @@
 import platform
 import shlex
-import subprocess
 import sys
 
 from test.examples_tools import run
@@ -8,14 +7,14 @@ from test.examples_tools import run
 run("conan install --profile ../../profiles/ros --build=missing")
 
 if platform.system() == "Windows":
-    cmake_py = subprocess.list2cmdline([f"-DPython3_EXECUTABLE={sys.executable}"])
+    cmake_py = f" -DPython3_EXECUTABLE={sys.executable}"
     run(
         r"call .\build\generators\conanrosenv.bat && "
-        f"colcon build --event-handlers console_cohesion+ --cmake-args {cmake_py}"
+        f'colcon build --event-handlers console_cohesion+ --cmake-args "{cmake_py}"'
     )
 else:
-    cmake_py = f"-DPython3_EXECUTABLE={shlex.quote(sys.executable)}"
+    cmake_py = f" -DPython3_EXECUTABLE={shlex.quote(sys.executable)}"
     run(
         f". ./build/Release/generators/conanrosenv.sh && "
-        f"colcon build --event-handlers console_cohesion+ --cmake-args {cmake_py}"
+        f'colcon build --event-handlers console_cohesion+ --cmake-args "{cmake_py}"'
     )
