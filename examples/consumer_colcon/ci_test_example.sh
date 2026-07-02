fc1492c3d671be165ccaed022acb288e6ec96e43
Carlos Zoido <mrgalleta@gmail.com>
2026-07-02 13:01:06 +0200

    debug 3

--- examples/consumer_colcon/ci_test_example.sh
+++ examples/consumer_colcon/ci_test_example.sh
@@ -6,4 +6,5 @@ python3 -m pip install -q --upgrade pip colcon-common-extensions catkin_pkg
 conan install --profile ../../profiles/ros --build=missing
 
 . ./build/Release/generators/conanrosenv.sh
+echo "[DEBUG] CMAKE_PREFIX_PATH right after sourcing conanrosenv.sh = $CMAKE_PREFIX_PATH"
 colcon build --event-handlers console_cohesion+ --cmake-args -DPython3_EXECUTABLE="$(which python3)"
