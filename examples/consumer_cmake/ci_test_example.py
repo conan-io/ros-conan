1fe9d98322eb1814c4068736546af4a2a8756a9f
Daniel <danimanzaneque@gmail.com>
2026-08-06 12:58:37 +0200

    Use conanfile.txt in consumer cmake example (#29)
    * Use conanfile.txt in consumer cmake example
    
    * remove vcvars
    
    * Apply suggestion from @danimtb

--- examples/consumer_cmake/ci_test_example.py
+++ examples/consumer_cmake/ci_test_example.py
@@ -2,4 +2,14 @@ import platform
 from test.examples_tools import run
 
 run("conan install --profile ../../profiles/ros --build=missing")
-run("conan build --profile ../../profiles/ros")
+
+if platform.system() == "Windows":
+    run(
+        r"call .\build\generators\conanbuild.bat && "
+        r"cmake --preset conan-default && cmake --build --preset conan-release"
+    )
+else:
+    run(
+        ". ./build/Release/generators/conanbuild.sh && "
+        "cmake --preset conan-release && cmake --build --preset conan-release"
+    )
